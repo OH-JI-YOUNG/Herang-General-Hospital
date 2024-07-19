8e613ddd69ab574cf74f400c1f0693cc75bf9221
OH-JI-YOUNG <ohjiyoung040997@gmail.com>
2024-07-19 15:54:49 +0900

    Add files via upload

--- js/script_1.js
+++ js/script_1.js
@@ -45,3 +45,45 @@ $( document ).ready(function() {
      return false;
   });
 });
+
+/*햄부기*/
+$(function () {
+  $("#slide-open").click(function () {
+    if ($("#burgur").hasClass("on")) {
+      $("#burgur").removeClass("on");
+      $("#slide").removeClass("on");
+    } else {
+      $("#burgur").addClass("on");
+      $("#slide").addClass("on");
+    }
+  });
+  $("#slide ul li a").click(function () {
+    if ($("#burgur").hasClass("on")) {
+      $("#burgur").removeClass("on");
+      $("#slide").removeClass("on");
+    } else {
+      $("#burgur").addClass("on");
+      $("#slide").addClass("on");
+    }
+  });
+
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          500
+        ); //움직이는 시간 조정
+        return false;
+      }
+    }
+  });
+});
